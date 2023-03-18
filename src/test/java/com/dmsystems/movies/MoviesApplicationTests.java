e9faeb5b4f056de3928a235284b31e2a9ca8ed3e
dmartins <external.delson.martins@allianz.com>
2023-03-18 16:22:06 -0300

    first commit

---
+++ src/test/java/com/dmsystems/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmsystems.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
